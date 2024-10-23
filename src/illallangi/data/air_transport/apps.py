16be7716375dc4fec72f60e89dfc1e37c837d3f5
Andrew Cole <andrew.cole@illallangi.com>
2024-10-23 23:19:36 +1100

    feat(illallangi-django-data): updated to use django.models for model list

--- src/illallangi/data/air_transport/apps.py
+++ src/illallangi/data/air_transport/apps.py
@@ -1,50 +1,63 @@
 from django.apps import AppConfig
 from django.conf import settings
+from django.db.models.signals import post_migrate
 
-from illallangi.django.data.models.models_manager import ModelsManager
 from illallangi.tripit.adapters import AirTransportAdapter as TripItAdapter
+
+
+def add_model(
+    **_kwargs: dict[str, object],
+) -> None:
+    from illallangi.django.data.models import Model, Synchronize
+
+    Model.objects.update_or_create(
+        description="Every leg of a journey, no matter how long or short, brings you closer to your destination.",
+        icon="air_transport/flights.jpg",
+        model="illallangi.data.air_transport.models.Flight",
+        plural="Flights",
+        singular="Flight",
+        url="flights_html",
+    )
+    Model.objects.update_or_create(
+        description="Each trip is a step towards discovering new horizons, embracing diverse cultures, and enriching your soul.",
+        icon="air_transport/trips.jpg",
+        model="illallangi.data.air_transport.models.Trip",
+        plural="Trips",
+        singular="Trip",
+        url="trips_html",
+    )
+
+    Synchronize.objects.update_or_create(
+        callable="illallangi.data.air_transport.apps.synchronize",
+    )
 
 
 class AirTransportConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "illallangi.data.air_transport"
 
-    def ready(self) -> None:
-        ModelsManager().add_model(
-            singular="Flight",
-            plural="Flights",
-            model="illallangi.data.air_transport.models.Flight",
-            url="flights_html",
-            icon="air_transport/flights.jpg",
-            description="Every leg of a journey, no matter how long or short, brings you closer to your destination.",
-        )
-        ModelsManager().add_model(
-            singular="Trip",
-            plural="Trips",
-            model="illallangi.data.air_transport.models.Trip",
-            url="trips_html",
-            icon="air_transport/trips.jpg",
-            description="Each trip is a step towards discovering new horizons, embracing diverse cultures, and enriching your soul.",
+    def ready(
+        self,
+    ) -> None:
+        post_migrate.connect(
+            add_model,
+            sender=self,
         )
 
-        def synchronize() -> None:
-            from illallangi.data.air_transport.adapters import (
-                AirTransportAdapter as DjangoAdapter,
-            )
 
-            src = TripItAdapter(
-                **settings.TRIPIT,
-            )
-            dst = DjangoAdapter()
+def synchronize() -> None:
+    from illallangi.data.air_transport.adapters import (
+        AirTransportAdapter as DjangoAdapter,
+    )
 
-            src.load(
-                **settings.AIR_TRANSPORT,
-            )
-            dst.load()
+    src = TripItAdapter(
+        **settings.TRIPIT,
+    )
+    dst = DjangoAdapter()
 
-            src.sync_to(dst)
+    src.load(
+        **settings.AIR_TRANSPORT,
+    )
+    dst.load()
 
-        ModelsManager().add_synchronize(
-            name=__name__,
-            synchronize=synchronize,
-        )
+    src.sync_to(dst)
